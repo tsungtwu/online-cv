94f3e4dc3f61edf8afd927bede4245564bb28d38
tsungwu <jtrmn.wu@gmail.com>
2017-04-26 01:41:00 +0800

    git init

---
+++ _site/assets/js/main.js
@@ -0,0 +1,37 @@
+jQuery(document).ready(function($) {
+
+    $('.level-bar-inner').css('width', '0');
+    
+    $(window).on('load', function() {
+
+        $('.level-bar-inner').each(function() {
+        
+            var itemWidth = $(this).data('level');
+            
+            $(this).animate({
+                width: itemWidth
+            }, 800);
+            
+        });
+
+    });
+
+
+    $(window).scroll(function() {
+			if ($(this).scrollTop() > 100) {
+				$('.scroll-up').fadeIn();
+			} else {
+				$('.scroll-up').fadeOut();
+			}
+		});
+    $('.scroll-up').click(function() {
+    
+        $('html, body').animate({
+          scrollTop: 0
+        }, 1000);
+        return false;
+      
+  });
+    
+
+});
